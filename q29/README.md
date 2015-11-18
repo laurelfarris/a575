f15917d37b59f4c1e360fe3f47fbad7412682035
Laurel Farris <laurel@laurelfarris.com>
2015-11-18 12:04:42 -0700

    hw

--- q29/README.md
+++ q29/README.md
@@ -7,7 +7,7 @@ mass 100 solar masses, write a routine that generates
 random deviates to populate such a distribution. Use your routine to
 general some plots of IMFs for several different sample sizes, which
 you should plot in histogram form. Overlay the Salpeter function on
-your plots. 
+your plots.
 
 11/12/15
 Started... Not sure how to generate random numbers based on a specific
@@ -20,3 +20,7 @@ sample).
 11/15/15
 Getting there, but need to set x min and max to cover the proper range
 in masses.
+
+11/18/15
+Plots look correct (I think). Need to plot the Salpeter trend correctly,
+and figure out how to manage axis labels (ie keep them from overlapping).
--- q29/q29.py
+++ q29/q29.py
@@ -1,36 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pdb
+import matplotlib
+matplotlib.rc('font',**{'family':'serif','serif':['Times']})
+matplotlib.rc('text',usetex=True)
 
 # The Salpeter IMF gives a constant of proportionality of 0.53 when
 # the integral from 0.5 to 100 M is set equal to one.
 
 # IMF distribution:
 def IMF(mass):
-    return 0.53*mass**(-2.35)
+    #return 0.53*mass**(-2.35)
+    return (0.53*(10**mass)**(-2.35))
+    #return np.log10(0.53*(10**mass)**(-2.35))
 
-def inverse_IMF(y):
-    y = np.array(y)
+# Transformation method
+def trans(y):
     return (y/0.53)**(-1./2.35)
 
-# Returns values of y that follow the Salpeter IMF
-def transformed(y):
-    return (-1.35*y/0.53)**(-1./1.35)
-
 # Plot several different sample sizes (total number of stars)
-samples=[10,100,1000,10000]
-ynew = []
-xnew = []
-y = []
+N=[100,1000,10000,100000]
 
-# Loop through a few different sample sizes
-for N in range(0,len(samples)):
-    ynew.append(transformed(np.random.random(samples[N])))
-    xnew.append(inverse_IMF(ynew))
+# Create arrays of random values between 0 and 1 ('dN/dM')
+y1 = np.random.random(N[0])
+y2 = np.random.random(N[1])
+y3 = np.random.random(N[2])
+y4 = np.random.random(N[3])
 
-# convert lists to numpy arrays
-xnew = np.array(xnew)
-ynew = np.array(ynew)
+# Get arrays of masses back, between 0.5 and 100 (M_sun)
+x1 = (trans(y1))
+x2 = (trans(y2))
+x3 = (trans(y3))
+x4 = (trans(y4))
+
+# Convert x arrays to log (separate because this might not work).
+x1 = np.log10(x1)
+x2 = np.log10(x2)
+x3 = np.log10(x3)
+x4 = np.log10(x4)
 
 # Create axes objects for all four samples
 fig=plt.figure()
@@ -40,13 +47,39 @@ ax3=fig.add_subplot(223)
 ax4=fig.add_subplot(224)
 
 # Plot the histograms on one figure, with the Salpter IMF plotted over each one.
-ax1.hist(xnew[0],bins=50)
-ax1.plot(IMF(xnew[0]))
-ax2.hist(xnew[1],bins=50)
-ax2.plot(IMF(xnew[1]))
-ax3.hist(xnew[2],bins=50)
-ax3.plot(IMF(xnew[2]))
-ax4.hist(xnew[3],bins=50)
-ax4.plot(IMF(xnew[3]))
+ax1.hist((x1),bins=50)
+ax2.hist((x2),bins=50)
+ax3.hist((x3),bins=50)
+ax4.hist((x4),bins=50)
 
+'''
+# Plot the Salpeter IMF on top of each histogram
+ax1.plot(x1,IMF(x1))
+ax2.plot(x2,IMF(x2))
+ax3.plot(x3,IMF(x3))
+ax4.plot(x4,IMF(x4))
+'''
+
+# Make plots presentable
+ax1.set_xlim(min(x1),max(x1))
+ax2.set_xlim(min(x2),max(x2))
+ax3.set_xlim(min(x3),max(x3))
+ax4.set_xlim(min(x4),max(x4))
+
+ax1.set_xlabel('mass (M$_{\odot}$)')
+ax1.set_ylabel('counts')
+ax1.set_title('Sample of 100')
+ax2.set_xlabel('mass (M$_{\odot}$)')
+ax2.set_ylabel('counts')
+ax2.set_title('Sample of 1,000')
+ax3.set_xlabel('mass (M$_{\odot}$)')
+ax3.set_ylabel('counts')
+ax3.set_title('Sample of 10,000')
+ax4.set_xlabel('mass (M$_{\odot}$)')
+ax4.set_ylabel('counts')
+ax4.set_title('Sample of 100,000')
+# Save the figure
 plt.savefig('hist.png')
+
+
+
